aca6dc40c7e8e2d8287b5331881854b102307cd2
Jannis Mattheis <contact@jmattheis.de>
2018-12-09 11:11:28 +0100

    Add tag mutations & queries

---
+++ tag/mutations.go
@@ -0,0 +1,67 @@
+package tag
+
+import (
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+	"github.com/jinzhu/gorm"
+	"github.com/traggo/server/schema"
+)
+
+func Mutations(db *gorm.DB) graphql.Fields {
+	return graphql.Fields{
+		"createTag": createTag(db),
+		"removeTag": removeTag(db),
+	}
+}
+
+func removeTag(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: schema.TagDefinitionSchema,
+		Args: graphql.FieldConfigArgument{
+			"key": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			definition := schema.TagDefinition{Key: p.Args["key"].(string)}
+			if db.Find(&definition).RecordNotFound() {
+				return nil, fmt.Errorf("tag with key %s does not exist", definition.Key)
+			}
+
+			remove := db.Delete(&definition)
+			return definition, remove.Error
+		},
+	}
+}
+
+func createTag(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: schema.TagDefinitionSchema,
+		Args: graphql.FieldConfigArgument{
+			"key": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"color": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"type": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(schema.TagDefinitionTypeSchema),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			definition := &schema.TagDefinition{
+				Key:   p.Args["key"].(string),
+				Color: p.Args["color"].(string),
+				Type:  p.Args["type"].(schema.TagDefinitionType),
+			}
+
+			if !db.Find(definition).RecordNotFound() {
+				return nil, fmt.Errorf("tag with key %s does already exist", definition.Key)
+			}
+
+			create := db.Create(&definition)
+			return definition, create.Error
+		},
+	}
+}
---
+++ tag/queries.go
@@ -0,0 +1,42 @@
+package tag
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/graphql-go/graphql"
+	"github.com/traggo/server/schema"
+)
+
+func Queries(db *gorm.DB) graphql.Fields {
+	return graphql.Fields{
+		"tags":           getTags(db),
+		"suggestTag": suggestTag(db),
+	}
+}
+
+func suggestTag(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: graphql.NewList(schema.TagDefinitionSchema),
+		Args: graphql.FieldConfigArgument{
+			"query": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			query := p.Args["query"].(string)
+			var suggestions []schema.TagDefinition
+			find := db.Where("Key LIKE ?", query+"%").Find(&suggestions)
+			return suggestions, find.Error
+		},
+	}
+}
+
+func getTags(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: graphql.NewList(schema.TagDefinitionSchema),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			var tags []schema.TagDefinition
+			db.Find(&tags)
+			return tags, nil
+		},
+	}
+}
