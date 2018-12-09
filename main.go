b0da718930b6903f58a0b09695a3b922a19e4f5b
Jannis Mattheis <contact@jmattheis.de>
2018-12-09 11:12:04 +0100

    Add http server main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/traggo/server/database"
+	"github.com/traggo/server/gql"
+)
+
+func main() {
+	// TODO configurable
+	db, err := database.New("sqlite3", "file::memory:?mode=memory&cache=shared")
+	db.LogMode(true)
+	if err != nil {
+		panic(err)
+	}
+
+	http.Handle("/graphql", gql.Handler(db))
+	http.ListenAndServe(":3030", nil) // TODO configurable port
+}
