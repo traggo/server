89f8af62ba9f12c085cb30aa2a1fe0c4820ebd30
Jannis Mattheis <contact@jmattheis.de>
2018-12-22 22:24:14 +0100

    Add password utils

---
+++ user/password/password_test.go
@@ -0,0 +1,21 @@
+package password
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPasswordSuccess(t *testing.T) {
+	password := CreatePassword("secret", 4)
+	assert.Equal(t, true, ComparePassword(password, []byte("secret")))
+}
+
+func TestPasswordFailure(t *testing.T) {
+	password := CreatePassword("secret", 4)
+	assert.Equal(t, false, ComparePassword(password, []byte("secretx")))
+}
+
+func TestBCryptFailure(t *testing.T) {
+	assert.Panics(t, func() { CreatePassword("secret", 12312) })
+}
