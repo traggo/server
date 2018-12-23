9b8a8222afaba570922812a5e62195c97af17f25
Jannis Mattheis <contact@jmattheis.de>
2018-12-23 17:22:04 +0100

    Init the logger in main

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/traggo/server/database"
 	"github.com/traggo/server/gql"
+	"github.com/traggo/server/logger"
 )
 
 func main() {
 	// TODO configurable
+	logger.Init(zerolog.DebugLevel)
 	db, err := database.New("sqlite3", "file::memory:?mode=memory&cache=shared")
-	db.LogMode(true)
 	if err != nil {
-		panic(err)
+		log.Fatal().Err(err).Msg("Failed to connect to the database")
 	}
 
 	http.Handle("/graphql", gql.Handler(db, 10))
