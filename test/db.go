9d241b4c668c272745db1812770bac2335f3e1bf
Jannis Mattheis <contact@jmattheis.de>
2019-01-06 15:20:40 +0100

    Create helper methods for adding a user

--- test/db.go
+++ test/db.go
@@ -1,14 +1,44 @@
 package test
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/traggo/server/database"
+	"github.com/traggo/server/model"
 )
 
 // InMemoryDB create a in memory database for testing.
-func InMemoryDB(t assert.TestingT) *gorm.DB {
+func InMemoryDB(t assert.TestingT) *Database {
 	db, err := database.New("sqlite3", "file::memory:?mode=memory&cache=shared")
 	assert.Nil(t, err)
-	return db
+	return &Database{DB: db}
+}
+
+// Database wraps the gorm.DB and provides helper methods
+type Database struct {
+	*gorm.DB
+}
+
+// User creates a user
+func (d *Database) User(id int) {
+	d.NewUser(id, fmt.Sprint("test", id), false)
+}
+
+// NewUser creates a user
+func (d *Database) NewUser(id int, name string, admin bool) model.User {
+	return d.NewUserPass(id, name, []byte{1, 2, 3}, admin)
+}
+
+// NewUser creates a user
+func (d *Database) NewUserPass(id int, name string, pass []byte, admin bool) model.User {
+	user := model.User{
+		ID:    id,
+		Name:  name,
+		Pass:  pass,
+		Admin: admin,
+	}
+	d.Create(&user)
+	return user
 }
--- test/db_test.go
+++ test/db_test.go
@@ -1,13 +1,32 @@
-package test
+package test_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/traggo/server/model"
+	"github.com/traggo/server/test"
 )
 
 func TestInMemoryDB(t *testing.T) {
-	db := InMemoryDB(t)
+	db := test.InMemoryDB(t)
 	defer db.Close()
 	assert.NotNil(t, db)
+}
+
+func TestUser(t *testing.T) {
+	db := test.InMemoryDB(t)
+	defer db.Close()
+	db.User(1)
+	db.NewUser(2, "abc", true)
+	db.NewUserPass(3, "xxx", []byte{5, 5}, true)
+
+	expected := []model.User{
+		{ID: 1, Name: "test1", Pass: []uint8{1, 2, 3}, Admin: false},
+		{ID: 2, Name: "abc", Pass: []uint8{1, 2, 3}, Admin: true},
+		{ID: 3, Name: "xxx", Pass: []uint8{5, 5}, Admin: true}}
+
+	var users []model.User
+	db.Find(&users)
+	assert.Equal(t, expected, users)
 }
