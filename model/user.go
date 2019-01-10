10f6e6dd7ae020d28d4b0674aab2f9c183bb85bc
Jannis Mattheis <contact@jmattheis.de>
2019-01-10 20:30:27 +0100

    Make username unique

--- model/user.go
+++ model/user.go
@@ -3,7 +3,7 @@ package model
 // User holds information about credentials and authorizations.
 type User struct {
 	ID    int    `gorm:"primary_key;unique_index;AUTO_INCREMENT"`
-	Name  string `gorm:"unique_index"`
+	Name  string `gorm:"unique"`
 	Pass  []byte
 	Admin bool
 }
