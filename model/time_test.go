ef14ff74ab3daebe61f69d45daafbb5cd9dd0524
Jannis Mattheis <contact@jmattheis.de>
2019-01-10 20:30:45 +0100

    Add model.Time for gql marshalling

---
+++ model/time_test.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTime_MarshalGQL(t *testing.T) {
+	var buffer bytes.Buffer
+	expected := "2009-06-30T18:30:00+02:00"
+	parse, err := time.Parse(time.RFC3339, expected)
+	assert.Nil(t, err)
+	toTest := Time(parse)
+	toTest.MarshalGQL(&buffer)
+	actual := buffer.String()
+	assert.Equal(t, expected, actual)
+}
+
+func TestTime_UnmarshalGQL_success(t *testing.T) {
+	date := "2009-06-30T18:30:00+02:00"
+	parse, err := time.Parse(time.RFC3339, date)
+	assert.Nil(t, err)
+	expected := Time(parse)
+
+	actual := &Time{}
+	err = actual.UnmarshalGQL(date)
+	assert.Nil(t, err)
+
+	assert.Equal(t, expected, *actual)
+}
+
+func TestTime_UnmarshalGQL_failInvalidType(t *testing.T) {
+	actual := &Time{}
+	err := actual.UnmarshalGQL(1)
+	assert.EqualError(t, err, "time must be a string")
+}
+
+func TestTime_UnmarshalGQL_invalidFormat(t *testing.T) {
+	actual := &Time{}
+	err := actual.UnmarshalGQL("lol")
+	assert.EqualError(t, err, `parsing time "lol" as "2006-01-02T15:04:05Z07:00": cannot parse "lol" as "2006"`)
+}
