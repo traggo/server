0265e5161624b0cd0875c0ae79d289a455cd6b7a
Jannis Mattheis <contact@jmattheis.de>
2019-01-12 19:48:36 +0100

    Add intermediate FutureLog struct

---
+++ config/error.go
@@ -0,0 +1,10 @@
+package config
+
+import "github.com/rs/zerolog"
+
+// FutureLog is an intermediate type for log messages. It is used before the config was loaded because without loaded
+// config we do not know the log level, so we log these messages once the config was initialized.
+type FutureLog struct {
+	Level zerolog.Level
+	Msg   string
+}
