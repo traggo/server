d5a2c208546272aefdf96e563d2020ddf021dc62
Jannis Mattheis <contact@jmattheis.de>
2019-03-16 21:19:29 +0100

    Add remove timespan

---
+++ timespan/remove_test.go
@@ -0,0 +1,56 @@
+package timespan
+
+import (
+	"testing"
+
+	"github.com/traggo/server/generated/gqlmodel"
+
+	"github.com/stretchr/testify/require"
+	"github.com/traggo/server/model"
+	"github.com/traggo/server/test"
+	"github.com/traggo/server/test/fake"
+)
+
+func TestRemoveTimeSpan_succeeds_removesTimeSpan(t *testing.T) {
+	db := test.InMemoryDB(t)
+	defer db.Close()
+	db.User(5)
+	db.Create(&model.TimeSpan{
+		ID:     1,
+		UserID: 3,
+	})
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	actual, err := resolver.RemoveTimeSpan(fake.User(3), 1)
+	require.NoError(t, err)
+	expected := &gqlmodel.TimeSpan{
+		ID: 1,
+	}
+	require.Equal(t, expected, actual)
+	assertTimeSpanCount(t, db, 0)
+}
+
+func TestRemoveTimeSpan_fails_notExistingTimeSpan(t *testing.T) {
+	db := test.InMemoryDB(t)
+	defer db.Close()
+	db.User(3)
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	_, err := resolver.RemoveTimeSpan(fake.User(3), 5)
+	require.EqualError(t, err, "timespan with id 5 does not exist")
+}
+
+func TestRemoveTimeSpan_fails_noPermission(t *testing.T) {
+	db := test.InMemoryDB(t)
+	defer db.Close()
+	db.User(3)
+	db.User(5)
+	db.Create(&model.TimeSpan{
+		ID:     1,
+		UserID: 3,
+	})
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	_, err := resolver.RemoveTimeSpan(fake.User(5), 1)
+	require.EqualError(t, err, "timespan with id 1 does not exist")
+}
