ed8029e63183c885ba8c5c6008b9c08853567f48
Jannis Mattheis <contact@jmattheis.de>
2019-03-16 23:44:35 +0100

    Add copy timespan

---
+++ timespan/copy.go
@@ -0,0 +1,21 @@
+package timespan
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/traggo/server/auth"
+	"github.com/traggo/server/generated/gqlmodel"
+	"github.com/traggo/server/model"
+)
+
+// CopyTimeSpan copies a time span.
+func (r *ResolverForTimeSpan) CopyTimeSpan(ctx context.Context, id int, start model.Time, end *model.Time) (*gqlmodel.TimeSpan, error) {
+	old := &model.TimeSpan{ID: id}
+
+	if r.DB.Preload("Tags").Where("user_id = ?", auth.GetUser(ctx).ID).Find(old).RecordNotFound() {
+		return nil, fmt.Errorf("time span with id %d does not exist", id)
+	}
+
+	return r.CreateTimeSpan(ctx, start, end, tagsToInputTag(old.Tags))
+}
---
+++ timespan/copy_test.go
@@ -0,0 +1,98 @@
+package timespan
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/traggo/server/generated/gqlmodel"
+	"github.com/traggo/server/model"
+	"github.com/traggo/server/test"
+	"github.com/traggo/server/test/fake"
+)
+
+func TestCopy_noPermission(t *testing.T) {
+	db := test.InMemoryDB(t)
+	db.User(3)
+	db.User(2)
+	db.Create(&model.TimeSpan{
+		ID:            3,
+		UserID:        3,
+		StartUserTime: test.Time("2019-06-10T18:30:00Z"),
+		StartUTC:      test.Time("2019-06-10T16:30:00Z"),
+		EndUserTime:   test.TimeP("2019-06-10T18:30:00Z"),
+		EndUTC:        test.TimeP("2019-06-10T16:30:00Z"),
+		Tags:          []model.TimeSpanTag{},
+	})
+	defer db.Close()
+	db.Create(&model.TagDefinition{Key: "test", UserID: 5})
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	timeSpan, err := resolver.CopyTimeSpan(fake.User(2), 3, test.ModelTime("2019-06-10T18:30:00+02:00"),
+		test.ModelTimeP("2019-06-10T18:35:00+02:00"))
+	require.Nil(t, timeSpan)
+	require.EqualError(t, err, "time span with id 3 does not exist")
+	assertTimeSpanCount(t, db, 1)
+}
+
+func TestCopy(t *testing.T) {
+	db := test.InMemoryDB(t)
+	db.User(3)
+	db.Create(&model.TagDefinition{Key: "test", UserID: 3})
+	db.Create(&model.TimeSpan{
+		ID:            3,
+		UserID:        3,
+		StartUserTime: test.Time("2019-06-10T18:30:00Z"),
+		StartUTC:      test.Time("2019-06-10T16:30:00Z"),
+		EndUserTime:   test.TimeP("2019-06-10T18:30:00Z"),
+		EndUTC:        test.TimeP("2019-06-10T16:30:00Z"),
+		Tags: []model.TimeSpanTag{
+			{Key: "test", TimeSpanID: 3},
+		},
+	})
+	defer db.Close()
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	timeSpan, err := resolver.CopyTimeSpan(fake.User(3), 3, test.ModelTime("2019-06-15T10:30:00+02:00"), nil)
+	require.NoError(t, err)
+
+	expected := &gqlmodel.TimeSpan{
+		ID:            4,
+		StartUserTime: test.ModelTime("2019-06-15T10:30:00Z"),
+		StartUtc:      test.ModelTime("2019-06-15T08:30:00Z"),
+		EndUserTime:   nil,
+		EndUtc:        nil,
+		Tags: []gqlmodel.TimeSpanTag{
+			{Key: "test"},
+		},
+	}
+
+	require.Equal(t, expected, timeSpan)
+
+	assertTimeSpanCount(t, db, 2)
+	assertTimeSpanExist(t, db, model.TimeSpan{
+		ID:            4,
+		UserID:        3,
+		StartUserTime: test.Time("2019-06-15T10:30:00Z"),
+		StartUTC:      test.Time("2019-06-15T08:30:00Z"),
+		EndUserTime:   nil,
+		EndUTC:        nil,
+		Tags: []model.TimeSpanTag{
+			{Key: "test", TimeSpanID: 4},
+		},
+	})
+}
+
+func TestCopy_notFound(t *testing.T) {
+	db := test.InMemoryDB(t)
+	db.User(3)
+	db.User(2)
+	defer db.Close()
+	db.Create(&model.TagDefinition{Key: "test", UserID: 5})
+
+	resolver := ResolverForTimeSpan{DB: db.DB}
+	timeSpan, err := resolver.CopyTimeSpan(fake.User(2), 3, test.ModelTime("2019-06-10T18:30:00+02:00"),
+		test.ModelTimeP("2019-06-10T18:35:00+02:00"))
+	require.Nil(t, timeSpan)
+	require.EqualError(t, err, "time span with id 3 does not exist")
+	assertTimeSpanCount(t, db, 0)
+}
