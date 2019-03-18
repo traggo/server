31339639c157a2a8d3ac24cc6445b875540a4433
Jannis Mattheis <contact@jmattheis.de>
2019-03-18 16:22:17 +0100

    Add root component

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Root} from './Root';
 
-ReactDOM.render(<div/>, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
