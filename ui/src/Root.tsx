70c3b5025293efdd604169e5a3bf5e6fa48dc5d3
Jannis Mattheis <contact@jmattheis.de>
2019-04-27 16:49:50 +0200

    Add router

--- ui/src/Root.tsx
+++ ui/src/Root.tsx
@@ -1,79 +1,26 @@
-import React from 'react';
-import {LoginPage} from './login/LoginPage';
+import * as React from 'react';
 import './global.css';
 import 'typeface-roboto';
-import Query, {QueryResult} from 'react-apollo/Query';
-import Mutation from 'react-apollo/Mutation';
-import Button from '@material-ui/core/Button';
-import * as gqlUser from './gql/user';
-import Typography from '@material-ui/core/Typography';
-import {Logout} from './gql/__generated__/Logout';
-import {CurrentUser} from './gql/__generated__/CurrentUser';
 import {ThemeProvider} from './provider/ThemeProvider';
 import {ApolloProvider} from './provider/ApolloProvider';
 import {SnackbarProvider} from './provider/SnackbarProvider';
-import {CenteredSpinner} from './common/CenteredSpinner';
-import Grid from '@material-ui/core/Grid';
-import {DefaultPaper} from './common/DefaultPaper';
-import {ApolloError} from 'apollo-client/errors/ApolloError';
+import {MuiPickersUtilsProvider} from 'material-ui-pickers/';
+import MomentUtils from '@date-io/moment';
+import {Router} from './Router';
+import {HashRouter} from 'react-router-dom';
 
 export const Root = () => {
     return (
         <ApolloProvider>
             <ThemeProvider>
-                <SnackbarProvider>
-                    <Query<CurrentUser> query={gqlUser.CurrentUser}>
-                        {({loading, error, data, refetch}: QueryResult<CurrentUser>) => {
-                            if (loading) {
-                                return <CenteredSpinner />;
-                            }
-                            if (error) {
-                                return <Error refetch={refetch} error={error} />;
-                            }
-                            if (!data || data.user === null) {
-                                return <LoginPage />;
-                            } else {
-                                return (
-                                    <React.Fragment>
-                                        <Typography>Hello {data.user.name}</Typography>
-                                        <Mutation<Logout>
-                                            mutation={gqlUser.Logout}
-                                            update={(cache) =>
-                                                cache.writeQuery<CurrentUser>({
-                                                    query: gqlUser.CurrentUser,
-                                                    data: {user: null},
-                                                })
-                                            }>
-                                            {(logout) => <Button onClick={() => logout()}>Logout</Button>}
-                                        </Mutation>
-                                    </React.Fragment>
-                                );
-                            }
-                        }}
-                    </Query>
-                </SnackbarProvider>
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <SnackbarProvider>
+                        <HashRouter>
+                            <Router />
+                        </HashRouter>
+                    </SnackbarProvider>
+                </MuiPickersUtilsProvider>
             </ThemeProvider>
         </ApolloProvider>
-    );
-};
-
-const Error: React.FC<{error: ApolloError; refetch: () => void}> = ({error, refetch}) => {
-    return (
-        <Grid container direction="row" alignItems="center" justify="center" style={{height: '100%'}}>
-            <Grid item>
-                <DefaultPaper>
-                    <Typography variant="h3" component="h1" gutterBottom={true}>
-                        Error
-                    </Typography>
-                    <Typography component="p">
-                        {error.networkError && error.networkError.name + ': ' + error.networkError.message}
-                        {error.graphQLErrors.map((gqlError) => gqlError.message).join(', ')}
-                    </Typography>
-                    <Button style={{marginTop: 10}} size="large" variant="outlined" onClick={() => refetch()}>
-                        Retry
-                    </Button>
-                </DefaultPaper>
-            </Grid>
-        </Grid>
     );
 };
---
+++ ui/src/Router.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import {useQuery} from 'react-apollo-hooks';
+import {CurrentUser} from './gql/__generated__/CurrentUser';
+import * as gqlUser from './gql/user';
+import {CenteredSpinner} from './common/CenteredSpinner';
+import {LoginPage} from './login/LoginPage';
+import {Typography} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import {ApolloError} from 'apollo-boost';
+import Grid from '@material-ui/core/Grid';
+import {DefaultPaper} from './common/DefaultPaper';
+import {Page} from './common/Page';
+import {Redirect, Route, Switch} from 'react-router';
+import {DailyPage} from './timespan/DailyPage';
+
+export const Router = () => {
+    const {loading, error, data, refetch} = useQuery<CurrentUser>(gqlUser.CurrentUser);
+    if (loading) {
+        return <CenteredSpinner />;
+    }
+    if (error) {
+        return <Error refetch={refetch} error={error} />;
+    }
+    const loggedIn = data && data.user;
+
+    return (
+        <Switch>
+            <Route path="/user/login">{loggedIn ? <Redirect to="/" /> : <LoginPage />}</Route>
+            {loggedIn ? null : <Redirect to="/user/login" />}
+
+            <Page>
+                <Route exact path="/dashboard" component={TODO} />
+                <Route exact path="/timesheet/daily" component={DailyPage} />
+                <Route exact path="/timesheet/weekly" component={TODO} />
+                <Route exact path="/user/settings" component={TODO} />
+                <Route exact path="/user/devices" component={TODO} />
+                <Route exact path="/admin/users" component={TODO} />
+                <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
+            </Page>
+        </Switch>
+    );
+};
+
+const Error: React.FC<{error: ApolloError; refetch: () => void}> = ({error, refetch}) => {
+    return (
+        <Grid container direction="row" alignItems="center" justify="center" style={{height: '100%'}}>
+            <Grid item>
+                <DefaultPaper>
+                    <Typography variant="h3" component="h1" gutterBottom={true}>
+                        Error
+                    </Typography>
+                    <Typography component="p">
+                        {error.networkError && error.networkError.name + ': ' + error.networkError.message}
+                        {error.graphQLErrors.map((gqlError) => gqlError.message).join(', ')}
+                    </Typography>
+                    <Button style={{marginTop: 10}} size="large" variant="outlined" onClick={() => refetch()}>
+                        Retry
+                    </Button>
+                </DefaultPaper>
+            </Grid>
+        </Grid>
+    );
+};
+
+export const TODO = () => {
+    return (
+        <Typography align="center" variant={'h2'}>
+            TODO
+        </Typography>
+    );
+};
