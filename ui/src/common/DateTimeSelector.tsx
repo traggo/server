78cb6ca156bfe3b7da45a0a9b1443506eee75cdb
Jannis Mattheis <contact@jmattheis.de>
2019-11-29 09:38:43 +0100

    Reduce rerenders

--- ui/src/common/DateTimeSelector.tsx
+++ ui/src/common/DateTimeSelector.tsx
@@ -11,41 +11,37 @@ interface DateTimeSelectorProps {
     popoverOpen?: (open: boolean) => void;
 }
 
-export const DateTimeSelector: React.FC<DateTimeSelectorProps> = ({
-    selectedDate,
-    onSelectDate,
-    showDate,
-    label,
-    popoverOpen = () => {},
-}) => {
-    return (
-        <KeyboardDateTimePicker
-            variant="inline"
-            InputProps={{disableUnderline: true}}
-            title={selectedDate.format()}
-            style={{width: showDate ? 185 : 95}}
-            PopoverProps={{onEntered: () => popoverOpen(true), onExited: () => popoverOpen(false)}}
-            margin="none"
-            value={uglyConvertToLocalTime(selectedDate).format()}
-            onChange={(date: moment.Moment) => {
-                if (!showDate) {
-                    date = date.set({
-                        day: selectedDate.day(),
-                        month: selectedDate.month(),
-                        year: selectedDate.year(),
-                    });
-                }
-                if (uglyConvertToLocalTime(selectedDate).isSame(date)) {
-                    return;
-                }
+export const DateTimeSelector: React.FC<DateTimeSelectorProps> = React.memo(
+    ({selectedDate, onSelectDate, showDate, label, popoverOpen = () => {}}) => {
+        return (
+            <KeyboardDateTimePicker
+                variant="inline"
+                InputProps={{disableUnderline: true}}
+                title={selectedDate.format()}
+                style={{width: showDate ? 185 : 95}}
+                PopoverProps={{onEntered: () => popoverOpen(true), onExited: () => popoverOpen(false)}}
+                margin="none"
+                value={uglyConvertToLocalTime(selectedDate).format()}
+                onChange={(date: moment.Moment) => {
+                    if (!showDate) {
+                        date = date.set({
+                            day: selectedDate.day(),
+                            month: selectedDate.month(),
+                            year: selectedDate.year(),
+                        });
+                    }
+                    if (uglyConvertToLocalTime(selectedDate).isSame(date)) {
+                        return;
+                    }
 
-                onSelectDate(date);
-            }}
-            ampm={false}
-            format={showDate ? 'YYYY/MM/DD HH:mm' : 'HH:mm'}
-            label={label}
-            openTo={showDate ? 'date' : 'hours'}
-            onError={console.log}
-        />
-    );
-};
+                    onSelectDate(date);
+                }}
+                ampm={false}
+                format={showDate ? 'YYYY/MM/DD HH:mm' : 'HH:mm'}
+                label={label}
+                openTo={showDate ? 'date' : 'hours'}
+                onError={console.log}
+            />
+        );
+    }
+);
--- ui/src/timespan/TimeSpan.tsx
+++ ui/src/timespan/TimeSpan.tsx
@@ -38,132 +38,102 @@ export interface TimeSpanProps {
     addTagsToTracker?: (tags: TagSelectorEntry[]) => void;
 }
 
-export const TimeSpan: React.FC<TimeSpanProps> = ({
-    range: {from, to, oldFrom},
-    id,
-    initialTags,
-    dateSelectorOpen = () => {},
-    deleted = () => {},
-    stopped = () => {},
-    addTagsToTracker,
-}) => {
-    const [selectedEntries, setSelectedEntries] = React.useState<TagSelectorEntry[]>(initialTags);
-    const [openMenu, setOpenMenu] = useStateAndDelegateWithDelayOnChange<null | HTMLElement>(null, (o) => dateSelectorOpen(!!o));
-    const [stopTimer] = useMutation<StopTimer, StopTimerVariables>(gqlTimeSpan.StopTimer, {
-        update: (cache, {data}) => {
-            const oldTrackers = cache.readQuery<Trackers>({query: gqlTimeSpan.Trackers});
-            if (!oldTrackers || !data || !data.stopTimeSpan) {
-                return;
-            }
-            cache.writeQuery<Trackers>({
-                query: gqlTimeSpan.Trackers,
-                data: {
-                    timers: (oldTrackers.timers || []).filter((tracker) => tracker.id !== data.stopTimeSpan!.id),
-                },
-            });
-            addTimeSpanToCache(cache, data.stopTimeSpan);
-        },
-    });
-    const [startTimer] = useMutation<StartTimer, StartTimerVariables>(gqlTimeSpan.StartTimer, {
-        refetchQueries: [{query: gqlTimeSpan.Trackers}],
-    });
-    const [updateTimeSpan] = useMutation<UpdateTimeSpan, UpdateTimeSpanVariables>(gqlTimeSpan.UpdateTimeSpan);
-    const [removeTimeSpan] = useMutation<RemoveTimeSpan, RemoveTimeSpanVariables>(gqlTimeSpan.RemoveTimeSpan, {
-        update: (cache, {data}) => {
-            const oldData = cache.readQuery<TimeSpans>({query: gqlTimeSpan.TimeSpans});
-            const oldTrackers = cache.readQuery<Trackers>({query: gqlTimeSpan.Trackers});
-            if (!oldData || !oldTrackers || !data || !data.removeTimeSpan) {
-                return;
-            }
-            const removedId = data.removeTimeSpan.id;
-            cache.writeQuery<Trackers>({
-                query: gqlTimeSpan.Trackers,
-                data: {
-                    timers: (oldTrackers.timers || []).filter((tracker) => tracker.id !== removedId),
-                },
-            });
-            cache.writeQuery<TimeSpans>({
-                query: gqlTimeSpan.TimeSpans,
-                data: {
-                    timeSpans: {
-                        __typename: 'PagedTimeSpans',
-                        timeSpans: oldData.timeSpans.timeSpans.filter((ts) => ts.id !== removedId),
-                        cursor: oldData.timeSpans.cursor,
+export const TimeSpan: React.FC<TimeSpanProps> = React.memo(
+    ({
+        range: {from, to, oldFrom},
+        id,
+        initialTags,
+        dateSelectorOpen = () => {},
+        deleted = () => {},
+        stopped = () => {},
+        addTagsToTracker,
+    }) => {
+        const [selectedEntries, setSelectedEntries] = React.useState<TagSelectorEntry[]>(initialTags);
+        const [openMenu, setOpenMenu] = useStateAndDelegateWithDelayOnChange<null | HTMLElement>(null, (o) =>
+            dateSelectorOpen(!!o)
+        );
+        const [stopTimer] = useMutation<StopTimer, StopTimerVariables>(gqlTimeSpan.StopTimer, {
+            update: (cache, {data}) => {
+                const oldTrackers = cache.readQuery<Trackers>({query: gqlTimeSpan.Trackers});
+                if (!oldTrackers || !data || !data.stopTimeSpan) {
+                    return;
+                }
+                cache.writeQuery<Trackers>({
+                    query: gqlTimeSpan.Trackers,
+                    data: {
+                        timers: (oldTrackers.timers || []).filter((tracker) => tracker.id !== data.stopTimeSpan!.id),
                     },
-                },
-            });
-        },
-    });
+                });
+                addTimeSpanToCache(cache, data.stopTimeSpan);
+            },
+        });
+        const [startTimer] = useMutation<StartTimer, StartTimerVariables>(gqlTimeSpan.StartTimer, {
+            refetchQueries: [{query: gqlTimeSpan.Trackers}],
+        });
+        const [updateTimeSpan] = useMutation<UpdateTimeSpan, UpdateTimeSpanVariables>(gqlTimeSpan.UpdateTimeSpan);
+        const [removeTimeSpan] = useMutation<RemoveTimeSpan, RemoveTimeSpanVariables>(gqlTimeSpan.RemoveTimeSpan, {
+            update: (cache, {data}) => {
+                const oldData = cache.readQuery<TimeSpans>({query: gqlTimeSpan.TimeSpans});
+                const oldTrackers = cache.readQuery<Trackers>({query: gqlTimeSpan.Trackers});
+                if (!oldData || !oldTrackers || !data || !data.removeTimeSpan) {
+                    return;
+                }
+                const removedId = data.removeTimeSpan.id;
+                cache.writeQuery<Trackers>({
+                    query: gqlTimeSpan.Trackers,
+                    data: {
+                        timers: (oldTrackers.timers || []).filter((tracker) => tracker.id !== removedId),
+                    },
+                });
+                cache.writeQuery<TimeSpans>({
+                    query: gqlTimeSpan.TimeSpans,
+                    data: {
+                        timeSpans: {
+                            __typename: 'PagedTimeSpans',
+                            timeSpans: oldData.timeSpans.timeSpans.filter((ts) => ts.id !== removedId),
+                            cursor: oldData.timeSpans.cursor,
+                        },
+                    },
+                });
+            },
+        });
 
-    const wasMoved = !isSameDate(from, oldFrom);
-    const showDate = to !== undefined && (!isSameDate(from, to) || wasMoved);
-    return (
-        <Paper style={{display: 'flex', alignItems: 'center', padding: '10px', margin: '10px 0', opacity: wasMoved ? 0.5 : 1}}>
-            <div style={{flex: '1', marginRight: 10}}>
-                <TagSelector
-                    dialogOpen={dateSelectorOpen}
-                    selectedEntries={selectedEntries}
-                    onSelectedEntriesChanged={(entries) => {
-                        setSelectedEntries(entries);
-                        updateTimeSpan({
-                            variables: {
-                                oldStart: oldFrom,
-                                id,
-                                start: inUserTz(from).format(),
-                                end: to && inUserTz(to).format(),
-                                tags: toInputTags(entries),
-                            },
-                        });
-                    }}
-                />
-            </div>
-            <DateTimeSelector
-                popoverOpen={dateSelectorOpen}
-                selectedDate={from}
-                onSelectDate={(newFrom) => {
-                    if (!newFrom.isValid()) {
-                        return;
-                    }
-                    if (to && moment(newFrom).isAfter(to)) {
-                        const newTo = moment(newFrom).add(15, 'minute');
-                        updateTimeSpan({
-                            variables: {
-                                oldStart: oldFrom,
-                                id,
-                                start: inUserTz(newFrom).format(),
-                                end: inUserTz(newTo).format(),
-                                tags: toInputTags(selectedEntries),
-                            },
-                        });
-                    } else {
-                        updateTimeSpan({
-                            variables: {
-                                id,
-                                oldStart: oldFrom,
-                                start: inUserTz(newFrom).format(),
-                                end: to && inUserTz(to).format(),
-                                tags: toInputTags(selectedEntries),
-                            },
-                        });
-                    }
-                }}
-                showDate={showDate}
-                label="start"
-            />
-            {to !== undefined ? (
-                <DateTimeSelector
-                    popoverOpen={dateSelectorOpen}
-                    selectedDate={to}
-                    onSelectDate={(newTo) => {
-                        if (!newTo.isValid()) {
-                            return;
-                        }
-                        if (moment(newTo).isBefore(from)) {
-                            const newFrom = moment(newTo).subtract(15, 'minute');
+        const wasMoved = !isSameDate(from, oldFrom);
+        const showDate = to !== undefined && (!isSameDate(from, to) || wasMoved);
+        return (
+            <Paper
+                style={{display: 'flex', alignItems: 'center', padding: '10px', margin: '10px 0', opacity: wasMoved ? 0.5 : 1}}>
+                <div style={{flex: '1', marginRight: 10}}>
+                    <TagSelector
+                        dialogOpen={dateSelectorOpen}
+                        selectedEntries={selectedEntries}
+                        onSelectedEntriesChanged={(entries) => {
+                            setSelectedEntries(entries);
                             updateTimeSpan({
                                 variables: {
-                                    id,
                                     oldStart: oldFrom,
+                                    id,
+                                    start: inUserTz(from).format(),
+                                    end: to && inUserTz(to).format(),
+                                    tags: toInputTags(entries),
+                                },
+                            });
+                        }}
+                    />
+                </div>
+                <DateTimeSelector
+                    popoverOpen={dateSelectorOpen}
+                    selectedDate={from}
+                    onSelectDate={(newFrom) => {
+                        if (!newFrom.isValid()) {
+                            return;
+                        }
+                        if (to && moment(newFrom).isAfter(to)) {
+                            const newTo = moment(newFrom).add(15, 'minute');
+                            updateTimeSpan({
+                                variables: {
+                                    oldStart: oldFrom,
+                                    id,
                                     start: inUserTz(newFrom).format(),
                                     end: inUserTz(newTo).format(),
                                     tags: toInputTags(selectedEntries),
@@ -174,65 +144,102 @@ export const TimeSpan: React.FC<TimeSpanProps> = ({
                                 variables: {
                                     id,
                                     oldStart: oldFrom,
-                                    start: inUserTz(from).format(),
-                                    end: inUserTz(newTo).format(),
+                                    start: inUserTz(newFrom).format(),
+                                    end: to && inUserTz(to).format(),
                                     tags: toInputTags(selectedEntries),
                                 },
                             });
                         }
                     }}
                     showDate={showDate}
-                    label="end"
+                    label="start"
                 />
-            ) : (
-                <Button
-                    onClick={() => {
-                        stopTimer({variables: {id, end: inUserTz(moment()).format()}}).then(stopped);
-                    }}>
-                    Stop
-                </Button>
-            )}
-            <>
-                {
-                    <Typography
-                        variant="subtitle1"
-                        style={{width: 70, textAlign: 'right'}}
-                        title="The amount of time between from and to">
-                        {to ? <RelativeTime from={from} to={to} /> : <RelativeToNow from={from} />}
-                    </Typography>
-                }
-                <IconButton onClick={(e: React.MouseEvent<HTMLElement>) => setOpenMenu(e.currentTarget)}>
-                    <MoreVert />
-                </IconButton>
-                <Menu aria-haspopup="true" anchorEl={openMenu} open={openMenu !== null} onClose={() => setOpenMenu(null)}>
-                    {to ? (
-                        <MenuItem
-                            onClick={() => {
-                                setOpenMenu(null);
-                                startTimer({variables: {start: inUserTz(moment()).format(), tags: toInputTags(selectedEntries)}});
-                            }}>
-                            Continue
-                        </MenuItem>
-                    ) : null}
-                    {addTagsToTracker ? (
-                        <MenuItem
-                            onClick={() => {
-                                setOpenMenu(null);
-                                addTagsToTracker(selectedEntries);
-                            }}>
-                            Copy tags
-                        </MenuItem>
-                    ) : null}
-                    <MenuItem
+                {to !== undefined ? (
+                    <DateTimeSelector
+                        popoverOpen={dateSelectorOpen}
+                        selectedDate={to}
+                        onSelectDate={(newTo) => {
+                            if (!newTo.isValid()) {
+                                return;
+                            }
+                            if (moment(newTo).isBefore(from)) {
+                                const newFrom = moment(newTo).subtract(15, 'minute');
+                                updateTimeSpan({
+                                    variables: {
+                                        id,
+                                        oldStart: oldFrom,
+                                        start: inUserTz(newFrom).format(),
+                                        end: inUserTz(newTo).format(),
+                                        tags: toInputTags(selectedEntries),
+                                    },
+                                });
+                            } else {
+                                updateTimeSpan({
+                                    variables: {
+                                        id,
+                                        oldStart: oldFrom,
+                                        start: inUserTz(from).format(),
+                                        end: inUserTz(newTo).format(),
+                                        tags: toInputTags(selectedEntries),
+                                    },
+                                });
+                            }
+                        }}
+                        showDate={showDate}
+                        label="end"
+                    />
+                ) : (
+                    <Button
                         onClick={() => {
-                            setOpenMenu(null);
-                            removeTimeSpan({variables: {id}});
-                            deleted();
+                            stopTimer({variables: {id, end: inUserTz(moment()).format()}}).then(stopped);
                         }}>
-                        Delete
-                    </MenuItem>
-                </Menu>
-            </>
-        </Paper>
-    );
-};
+                        Stop
+                    </Button>
+                )}
+                <>
+                    {
+                        <Typography
+                            variant="subtitle1"
+                            style={{width: 70, textAlign: 'right'}}
+                            title="The amount of time between from and to">
+                            {to ? <RelativeTime from={from} to={to} /> : <RelativeToNow from={from} />}
+                        </Typography>
+                    }
+                    <IconButton onClick={(e: React.MouseEvent<HTMLElement>) => setOpenMenu(e.currentTarget)}>
+                        <MoreVert />
+                    </IconButton>
+                    <Menu aria-haspopup="true" anchorEl={openMenu} open={openMenu !== null} onClose={() => setOpenMenu(null)}>
+                        {to ? (
+                            <MenuItem
+                                onClick={() => {
+                                    setOpenMenu(null);
+                                    startTimer({
+                                        variables: {start: inUserTz(moment()).format(), tags: toInputTags(selectedEntries)},
+                                    });
+                                }}>
+                                Continue
+                            </MenuItem>
+                        ) : null}
+                        {addTagsToTracker ? (
+                            <MenuItem
+                                onClick={() => {
+                                    setOpenMenu(null);
+                                    addTagsToTracker(selectedEntries);
+                                }}>
+                                Copy tags
+                            </MenuItem>
+                        ) : null}
+                        <MenuItem
+                            onClick={() => {
+                                setOpenMenu(null);
+                                removeTimeSpan({variables: {id}});
+                                deleted();
+                            }}>
+                            Delete
+                        </MenuItem>
+                    </Menu>
+                </>
+            </Paper>
+        );
+    }
+);
