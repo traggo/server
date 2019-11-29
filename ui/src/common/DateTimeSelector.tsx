423eef811a1e69a307abdc21654998341b552257
Jannis Mattheis <contact@jmattheis.de>
2019-11-29 09:57:10 +0100

    Allow day change on date select

--- ui/src/common/DateTimeSelector.tsx
+++ ui/src/common/DateTimeSelector.tsx
@@ -13,17 +13,32 @@ interface DateTimeSelectorProps {
 
 export const DateTimeSelector: React.FC<DateTimeSelectorProps> = React.memo(
     ({selectedDate, onSelectDate, showDate, label, popoverOpen = () => {}}) => {
+        const [open, setOpen] = React.useState(false);
+
         return (
             <KeyboardDateTimePicker
                 variant="inline"
                 InputProps={{disableUnderline: true}}
                 title={selectedDate.format()}
                 style={{width: showDate ? 185 : 95}}
-                PopoverProps={{onEntered: () => popoverOpen(true), onExited: () => popoverOpen(false)}}
+                PopoverProps={{
+                    onEntered: () => {
+                        popoverOpen(true);
+                        setOpen(true);
+                    },
+                    onExited: () => {
+                        popoverOpen(false);
+                        setOpen(false);
+                    },
+                }}
                 margin="none"
                 value={uglyConvertToLocalTime(selectedDate).format()}
                 onChange={(date: moment.Moment) => {
-                    if (!showDate) {
+                    if (!date.isValid()) {
+                        return;
+                    }
+
+                    if (!showDate && !open) {
                         date = date.set({
                             date: selectedDate.date(),
                             month: selectedDate.month(),
