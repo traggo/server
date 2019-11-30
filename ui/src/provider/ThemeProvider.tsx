62b460bf9702310f4461779664fda3a3fe758ee9
Jannis Mattheis <contact@jmattheis.de>
2019-11-30 09:36:18 +0100

    Add themes

--- ui/src/provider/ThemeProvider.tsx
+++ ui/src/provider/ThemeProvider.tsx
@@ -1,60 +1,141 @@
 import * as React from 'react';
-import {MuiThemeProvider} from '@material-ui/core/styles';
+import {MuiThemeProvider, Theme} from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {Query, QueryResult} from 'react-apollo';
-import {Preferences, UserPreferences} from '../gql/preferences.local';
+import {useSettings} from '../gql/settings';
+import {Theme as SettingTheme} from '../gql/__generated__/globalTypes';
 
-const dark = createMuiTheme({
-    overrides: {
-        MuiLink: {
-            root: {
-                color: '#3498db',
+const themes: Record<SettingTheme, Theme> = {
+    [SettingTheme.GruvboxDark]: createMuiTheme({
+        overrides: {
+            MuiLink: {
+                root: {
+                    color: '#458588',
+                },
+            },
+            MuiIconButton: {
+                root: {
+                    color: 'inherit',
+                },
+            },
+            MuiListItemIcon: {
+                root: {
+                    color: 'inherit',
+                },
+            },
+            MuiToolbar: {
+                root: {
+                    background: '#a89984',
+                },
             },
         },
-    },
-    palette: {
-        primary: {
-            main: '#455a64',
+        palette: {
+            background: {
+                default: '#282828',
+                paper: '#32302f',
+            },
+            text: {
+                primary: '#fbf1d4',
+            },
+            primary: {
+                main: '#a89984',
+            },
+            secondary: {
+                main: '#f44336',
+            },
+            type: 'dark',
         },
-        secondary: {
-            main: '#f44336',
-        },
-        type: 'dark',
-    },
-});
-
-const light = createMuiTheme({
-    overrides: {
-        MuiLink: {
-            root: {
-                color: '#2980b9',
+    }),
+    [SettingTheme.GruvboxLight]: createMuiTheme({
+        overrides: {
+            MuiLink: {
+                root: {
+                    color: '#458588',
+                },
+            },
+            MuiIconButton: {
+                root: {
+                    color: 'inherit',
+                },
+            },
+            MuiListItemIcon: {
+                root: {
+                    color: 'inherit',
+                },
+            },
+            MuiToolbar: {
+                root: {
+                    background: '#7c6f64',
+                },
             },
         },
-    },
-    palette: {
-        background: {default: '#eeeeee'},
-        primary: {
-            main: '#455a64',
+        palette: {
+            background: {
+                default: '#fbf1c7',
+                paper: '#f9f5d7',
+            },
+            text: {
+                primary: '#282828',
+            },
+            primary: {
+                main: '#7c6f64',
+            },
+            secondary: {
+                main: '#f44336',
+            },
+            type: 'light',
         },
-        secondary: {
-            main: '#f44336',
+    }),
+    [SettingTheme.MaterialLight]: createMuiTheme({
+        overrides: {
+            MuiLink: {
+                root: {
+                    color: '#2980b9',
+                },
+            },
+            MuiIconButton: {
+                root: {
+                    color: 'inherit',
+                },
+            },
         },
-        type: 'light',
-    },
-});
+        palette: {
+            background: {default: '#eeeeee'},
+            primary: {
+                main: '#455a64',
+            },
+            secondary: {
+                main: '#f44336',
+            },
+            type: 'light',
+        },
+    }),
+    [SettingTheme.MaterialDark]: createMuiTheme({
+        overrides: {
+            MuiLink: {
+                root: {
+                    color: '#3498db',
+                },
+            },
+        },
+        palette: {
+            primary: {
+                main: '#455a64',
+            },
+            secondary: {
+                main: '#f44336',
+            },
+            type: 'dark',
+        },
+    }),
+};
 
 export const ThemeProvider: React.FC = ({children}) => {
+    const {theme} = useSettings();
     return (
-        <Query<UserPreferences> query={Preferences}>
-            {(pref: QueryResult<UserPreferences>) => {
-                return (
-                    <MuiThemeProvider theme={(pref.data && pref.data.theme === 'dark' && dark) || light}>
-                        <CssBaseline />
-                        {children}
-                    </MuiThemeProvider>
-                );
-            }}
-        </Query>
+        <MuiThemeProvider theme={themes[theme] || themes[SettingTheme.GruvboxDark]}>
+            <CssBaseline />
+            {children}
+        </MuiThemeProvider>
     );
 };
