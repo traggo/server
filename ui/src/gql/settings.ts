723cb8f0be45f8825a4852e107940edf820bd305
Jannis Mattheis <contact@jmattheis.de>
2019-11-30 09:35:00 +0100

    Add setting queries and helper hooks

---
+++ ui/src/gql/settings.ts
@@ -0,0 +1,38 @@
+import {gql} from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
+import {Settings as SettingsQueryResponse} from './__generated__/Settings';
+import {DateLocale, Theme, WeekDay} from './__generated__/globalTypes';
+import {stripTypename} from '../utils/strip';
+
+export const Settings = gql`
+    query Settings {
+        userSettings {
+            theme
+            dateLocale
+            firstDayOfTheWeek
+        }
+    }
+`;
+
+export const SetSettings = gql`
+    mutation SetSettings($settings: InputUserSettings!) {
+        setUserSettings(settings: $settings) {
+            theme
+        }
+    }
+`;
+
+const defaultSettings = {
+    theme: Theme.GruvboxDark,
+    dateLocale: DateLocale.English,
+    firstDayOfTheWeek: WeekDay.Monday,
+} as const;
+
+export const useSettings = (): {done: boolean} & Omit<SettingsQueryResponse['userSettings'], '__typename'> => {
+    const data = useQuery<SettingsQueryResponse>(Settings);
+
+    if (data.loading || data.error || !data.data) {
+        return {...defaultSettings, done: false};
+    }
+    return {done: true, ...stripTypename(data.data.userSettings)};
+};
