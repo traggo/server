d40f8eeea536d9390d16d1132aa61ccab26430c6
Jannis Mattheis <contact@jmattheis.de>
2024-11-03 13:49:33 +0100

    fixup! feat: total in dashboard entries

--- ui/src/dashboard/Entry/TagTooltip.tsx
+++ ui/src/dashboard/Entry/TagTooltip.tsx
@@ -1,4 +1,4 @@
-import {TooltipProps} from 'recharts';
+import {TooltipProps, TooltipPayload} from 'recharts';
 import {FInterval} from './dateformat';
 import Paper from '@material-ui/core/Paper';
 import {Typography} from '@material-ui/core';
@@ -11,23 +11,27 @@ export const TagTooltip = ({active, payload, dateFormat, total}: TooltipProps & 
         const first = payload[0];
         const start = dateFormat(moment(first.payload.start));
         const end = dateFormat(moment(first.payload.end));
-        let sum = 0;
 
         return (
             <Paper style={{padding: 10}} elevation={4}>
                 {first && <Typography variant={'subtitle2'}>{start === end ? `${start}` : `${start} - ${end}`}</Typography>}
-                {payload.map((entry) => {
-                    sum += entry.payload.data[entry.name] as number;
-                    return (
-                        <Typography key={entry.name}>
-                            {entry.name}: {prettyMs((entry.payload.data[entry.name] as number) * 1000)}
-                        </Typography>
-                    );
-                })}
-                {total ? <Typography>Total: {prettyMs(sum * 1000)}</Typography> : undefined}
+                {payload.map((entry) => (
+                    <Typography key={entry.name}>
+                        {entry.name}: {prettyMs((entry.payload.data[entry.name] as number) * 1000)}
+                    </Typography>
+                ))}
+                {total ? (
+                    <Typography>
+                        Total:
+                        {prettyMs(sum(payload) * 1000)}
+                    </Typography>
+                ) : undefined}
             </Paper>
         );
     }
 
     return null;
 };
+
+const sum = (payload: readonly TooltipPayload[]): number =>
+    payload.reduce((acc, entry) => (acc + entry.payload.data[entry.name]) as number, 0);
