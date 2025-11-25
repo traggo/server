b155bfe76aec23b314be0c9819cf87fbaebe4fe4
spl3g <spleefer6@yandex.ru>
2025-11-25 12:20:12 +0300

    feat: make TagChip use makeStyles

--- ui/src/common/TagChip.tsx
+++ ui/src/common/TagChip.tsx
@@ -2,24 +2,36 @@ import Chip from '@material-ui/core/Chip';
 import * as React from 'react';
 // @ts-ignore
 import bestContrast from 'get-best-contrast-color';
+import {makeStyles, Theme} from '@material-ui/core';
 
-export const TagChip = ({color, label}: {label: string; color: string}) => {
+const useStyles = makeStyles((theme: Theme) => ({
+    chip: {
+        margin: theme.spacing(0.5, 0.6),
+        cursor: 'text',
+        minHeight: '32px',
+        height: 'fit-content',
+        whiteSpace: 'normal',
+        wordBreak: 'break-word',
+    },
+}));
+
+interface TagChipProps {
+    label: string;
+    color: string;
+    onClick?: () => void;
+}
+
+export const TagChip: React.FC<TagChipProps> = ({color, label, onClick}) => {
+    const classes = useStyles();
     const textColor = bestContrast(color, ['#fff', '#000']);
     return (
         <Chip
             tabIndex={-1}
             variant="outlined"
-            style={{
-                background: color,
-                margin: '5px',
-                color: textColor,
-                cursor: 'text',
-                minHeight: '32px',
-                height: 'fit-content',
-                whiteSpace: 'normal',
-                wordBreak: 'break-word',
-            }}
+            className={classes.chip}
+            style={{background: color, color: textColor}}
             label={label}
+            onClick={onClick}
         />
     );
 };
